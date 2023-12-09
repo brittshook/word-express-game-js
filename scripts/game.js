d02e941747222a4d60581ca52f074d0f6d7bea7f
Britt Shook <brittanysshook@gmail.com>
2023-12-08 20:21:59 -0500

    add modal on cancel

--- scripts/game.js
+++ scripts/game.js
@@ -1,28 +1,28 @@
-import { stylePage, createWordTiles, showIncorrectGuesses } from "./style.js";
+import { stylePage, createWordTiles, showIncorrectGuesses, showModal } from "./style.js";
 
 const randomWordAPI = wordLength => `https://random-word-api.herokuapp.com/word?length=${wordLength}`;
 
 async function getRandomWord() {
-    try {
-      const randomWordArr = [];
+  try {
+    const randomWordArr = [];
 
-      for (let length = 3; length <= 7; length++) {
-        const response = await fetch(randomWordAPI(length));
-        const randomWord = await response.json();
-        randomWordArr.push(randomWord[0]);
-      }
-
-      return randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
-    } catch (error) {
-        console.log('Error:', error);
-        const wordArr = ['cozy', 'snuggle', 'puzzle', 'whisper', 'halcyon', 'dazzle', 'meadow', 'silly', 'giggly', 'harmony', 'bubble', 'mirth', 'quaint', 'serene', 'lively', 'jovial', 'bliss', 'azure', 'zoo', 'blithe'];
-        return wordArr[Math.floor(Math.random() * wordArr.length)];
+    for (let length = 3; length <= 7; length++) {
+      const response = await fetch(randomWordAPI(length));
+      const randomWord = await response.json();
+      randomWordArr.push(randomWord[0]);
     }
+
+    return randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+  } catch (error) {
+      console.log('Error:', error);
+      const wordArr = ['cozy', 'snuggle', 'puzzle', 'whisper', 'halcyon', 'dazzle', 'meadow', 'silly', 'giggly', 'harmony', 'bubble', 'mirth', 'quaint', 'serene', 'lively', 'jovial', 'bliss', 'azure', 'zoo', 'blithe'];
+      return wordArr[Math.floor(Math.random() * wordArr.length)];
+  }
 }
 
 async function getLetters() {
-    const randomWord = await getRandomWord();
-    return randomWord.split('');
+  const randomWord = await getRandomWord();
+  return randomWord.split('');
 }
 
 async function playGame() {
@@ -90,14 +90,20 @@ async function playGame() {
       }
     } else if (result === 'incorrect') {
       showIncorrectGuesses(currentGuess);
+    } else if (result === 'canceled') {
+      showModal();
     }
   }
 
   async function handleGuess() {
-    currentGuess = getUserGuess().trim();
+    currentGuess = getUserGuess();
 
-    if (/[a-zA-Z]/.test(currentGuess)) {
-      currentGuess = currentGuess.toLowerCase()[0];
+    if (currentGuess != null || currentGuess != undefined) {
+      currentGuess = currentGuess.trim();
+
+      if (/[a-zA-Z]/.test(currentGuess)) {
+        currentGuess = currentGuess.toLowerCase()[0];
+      }
     }
 
     checkGuess();
--- scripts/style.js
+++ scripts/style.js
@@ -5,13 +5,15 @@ function stylePage() {
     document.body.style.display = 'flex';
     document.body.style.justifyContent = 'center';
     document.body.style.alignItems = 'center';
-    
+    document.body.style.fontFamily = 'Armino, sans-serif';
+
+
     const app = document.querySelector('#app');
     app.style.display = 'flex';
     app.style.flexDirection = 'column';
     app.style.width = '82%';
     app.style.perspective = '10em';
-  
+
     const subwaySign = document.createElement('div');
     subwaySign.id = 'subway-sign';
     subwaySign.style.backgroundColor = 'var(--black, #20181A)';
@@ -23,36 +25,35 @@ function stylePage() {
     subwaySign.style.boxShadow = '0px 19px 16px -2px #00000066';
     subwaySign.style.borderRadius = '2px';
     app.appendChild(subwaySign);
-  
+
     const topEdgeOfSign = document.createElement('div');
     topEdgeOfSign.style.height = '16px';
     topEdgeOfSign.style.background = 'linear-gradient(#7A7A7C, #2E2F2F)';
     topEdgeOfSign.style.transform = 'translateY(2px) rotateX(39deg)';
     app.insertBefore(topEdgeOfSign, subwaySign);
-  
+
     const horizontalLine = document.createElement('hr');
     horizontalLine.style.backgroundColor = 'var(--white, #FEFEFE)';
     horizontalLine.style.height = '3px';
     horizontalLine.style.width = '100%';
     horizontalLine.style.marginTop = '40px';
     subwaySign.appendChild(horizontalLine);
-  
+
     const gameContainer = document.createElement('div');
     gameContainer.id = 'game-container';
     gameContainer.style.display = 'flex';
     gameContainer.style.flexDirection = 'column';
-    gameContainer.style.fontFamily = 'Armino, sans-serif';
     gameContainer.style.color = '#FEFEFE';
     gameContainer.style.padding = '0 20px 20px';
     gameContainer.style.gap = '20px';
     subwaySign.appendChild(gameContainer);
-  
+
     const headerContainer = document.createElement('div');
-    headerContainer. id = 'header-container';
+    headerContainer.id = 'header-container';
     headerContainer.style.display = 'flex';
     headerContainer.style.justifyContent = 'space-between';
     gameContainer.appendChild(headerContainer);
-  
+
     const h1 = document.createElement('h1');
     h1.style.fontSize = '78px';
     h1.style.fontWeight = '700';
@@ -61,8 +62,9 @@ function stylePage() {
     h1.style.maxWidth = '600px';
     h1.textContent = 'Word Express-Lexicon';
     headerContainer.appendChild(h1);
-  
+
     const startButton = document.createElement('button');
+    startButton.title = 'Play game';
     startButton.style.borderRadius = '100%';
     startButton.style.width = '175px';
     startButton.style.height = '175px';
@@ -73,8 +75,10 @@ function stylePage() {
     startButton.id = 'start';
     startButton.value = 'play game';
     startButton.innerHTML = '<img src="imgs/arrow.svg" alt="arrow pointing to top right corner" />';
+    startButton.onmouseover = () => startButton.style.backgroundColor = '#e5b002';
+    startButton.onmouseleave = () => startButton.style.backgroundColor = 'var(--yellow, #FFC501)';
     headerContainer.appendChild(startButton);
-  
+
     const incorrectGuessContainer = document.createElement('div');
     incorrectGuessContainer.id = 'guesses-container';
     incorrectGuessContainer.style.maxWidth = '300px';
@@ -87,45 +91,45 @@ function stylePage() {
     incorrectGuessContainer.style.textTransform = 'uppercase';
     incorrectGuessContainer.style.gap = '6px';
     headerContainer.appendChild(incorrectGuessContainer);
-  }
-  
-function createWordTiles(letterArr) {
-const gameContainer = document.querySelector('#game-container');
-
-const tileContainer = document.createElement('div');
-tileContainer.id = 'tile-container';
-tileContainer.style.display = 'none';
-tileContainer.style.gap = '20px';
-gameContainer.appendChild(tileContainer);
-
-for (let letter = 0; letter < letterArr.length; letter++) {
-    const tileEl = document.createElement('div');
-    tileContainer.appendChild(tileEl);
-    tileEl.style.borderRadius = '100%';
-    tileEl.style.height = '140px';
-    tileEl.style.width = '140px';
-    tileEl.style.display = 'flex';
-    tileEl.style.justifyContent = 'center';
-    tileEl.style.alignItems = 'center';
-    tileEl.style.textTransform = 'uppercase';
-    tileEl.style.fontSize = '48px';
-    tileEl.style.fontWeight = '700';
-
-    if (letter < 3) {
-        tileEl.style.backgroundColor = 'var(--blue, #3438C7)';
-    } else if (letter < 6) {
-        tileEl.style.backgroundColor = 'var(--yellow, #FFC501)';
-        tileEl.style.color = 'var(--black, #20181A)';
-    } else {
-        tileEl.style.backgroundColor = 'var(--pink, #A529A3)';
-    }
-
-    const letterEl = document.createElement('p');
-    letterEl.classList.add('letter');
-    letterEl.textContent = letterArr[letter];
-    letterEl.style.display = 'none';
-    tileEl.appendChild(letterEl);
 }
+
+function createWordTiles(letterArr) {
+    const gameContainer = document.querySelector('#game-container');
+
+    const tileContainer = document.createElement('div');
+    tileContainer.id = 'tile-container';
+    tileContainer.style.display = 'none';
+    tileContainer.style.gap = '20px';
+    gameContainer.appendChild(tileContainer);
+
+    for (let letter = 0; letter < letterArr.length; letter++) {
+        const tileEl = document.createElement('div');
+        tileContainer.appendChild(tileEl);
+        tileEl.style.borderRadius = '100%';
+        tileEl.style.height = '140px';
+        tileEl.style.width = '140px';
+        tileEl.style.display = 'flex';
+        tileEl.style.justifyContent = 'center';
+        tileEl.style.alignItems = 'center';
+        tileEl.style.textTransform = 'uppercase';
+        tileEl.style.fontSize = '48px';
+        tileEl.style.fontWeight = '700';
+
+        if (letter < 3) {
+            tileEl.style.backgroundColor = 'var(--blue, #3438C7)';
+        } else if (letter < 6) {
+            tileEl.style.backgroundColor = 'var(--yellow, #FFC501)';
+            tileEl.style.color = 'var(--black, #20181A)';
+        } else {
+            tileEl.style.backgroundColor = 'var(--pink, #A529A3)';
+        }
+
+        const letterEl = document.createElement('p');
+        letterEl.classList.add('letter');
+        letterEl.textContent = letterArr[letter];
+        letterEl.style.display = 'none';
+        tileEl.appendChild(letterEl);
+    }
 }
 
 function showIncorrectGuesses(currentGuess) {
@@ -149,7 +153,7 @@ function showIncorrectGuesses(currentGuess) {
 
     if (!allIncorrectGuesses.length) {
         incorrectGuessContainer.appendChild(incorrectGuess);
-    } else {    
+    } else {
         for (let guess = 0; guess < allIncorrectGuesses.length; guess++) {
             const guessText = allIncorrectGuesses[guess].textContent.toLowerCase();
 
@@ -167,4 +171,35 @@ function showIncorrectGuesses(currentGuess) {
     }
 }
 
-  export { stylePage, createWordTiles, showIncorrectGuesses };
+function showModal() {
+    const modal = document.createElement('div');
+    modal.style.position = 'fixed';
+    modal.style.height = '100%';
+    modal.style.width = '100%';
+    modal.style.display = 'flex';
+    modal.style.justifyContent = 'center';
+    modal.style.alignItems = 'center';
+    modal.style.flexDirection = 'column';
+    modal.style.gap = '14px';
+    modal.style.backgroundColor = 'var(--black-translucent, #20181A99)';
+    document.body.appendChild(modal);
+
+    const restartButton = document.createElement('button');
+    restartButton.title = 'Restart';
+    restartButton.style.width = '88px';
+    restartButton.style.height = '88px';
+    restartButton.style.backgroundColor = 'var(--off-white, #ede7e7)';
+    restartButton.style.border = 'none';
+    restartButton.style.borderRadius = '100%';
+    restartButton.style.display = 'flex';
+    restartButton.style.justifyContent = 'center';
+    restartButton.style.alignItems = 'center';
+    restartButton.style.cursor = 'pointer';
+    restartButton.innerHTML = '<img src="imgs/reload.svg" alt="restart"/>';
+    restartButton.onmouseover = () => restartButton.style.backgroundColor = '#ccc';
+    restartButton.onmouseleave = () => restartButton.style.backgroundColor = 'var(--off-white, #ede7e7)';
+    modal.addEventListener('click', () => location.reload());
+    modal.appendChild(restartButton);
+}
+
+export { stylePage, createWordTiles, showIncorrectGuesses, showModal };
